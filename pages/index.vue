c05b560f8a77e241aea42ec18065ccb05285be73
D3v3l0per1 <manuel.widmoser12@gmail.com>
2018-04-08 03:02:12 +0200

    Front End

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,49 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        storyblok-blog
-      </h1>
-      <h2 class="subtitle">
-        Awesome Blog
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+  <section id="posts">
+    <post-preview v-for="post in posts" :key="post.id" :title="post.title" :excerpt="post.previewText" :thumbnailImage="post.thumbnailURL" :id="post.id" />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostPreview from "@/components/Blog/PostPreview"
 
 export default {
   components: {
-    Logo
+    PostPreview
+  },
+  data () {
+    return {
+      posts: [
+        { 
+          title: "A new Beginning",
+          previewText: "This will be awesome, don't miss it!",
+          thumbnailURL: "https://cdn.shopify.com/s/files/1/0883/2790/products/StandardTiRando_1_2048x2048.jpg?v=1454709009",
+          id: "a-new-beginning"
+        },
+        { 
+          title: "A second Beginning",
+          previewText: "This will be shitty, don't miss it!",
+          thumbnailURL: "https://cdn.shopify.com/s/files/1/0883/2790/products/StandardTiRando_1_2048x2048.jpg?v=1454709009",
+          id: "a-second-beginning"
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
+#posts {
+  padding-top: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  flex-direction: column;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
+
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
 }
 </style>
